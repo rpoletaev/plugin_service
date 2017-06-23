069a10a945f399db0b639bc952e25a6bfc35c99f
losaped <losaped@gmail.com>
2017-06-23 14:08:04 +0300

    add marsshall indent to json

--- cmd/main.go
+++ cmd/main.go
@@ -128,7 +128,7 @@ func getExportJSONHandler(rw http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	objJSON, err := json.Marshal(obj)
+	objJSON, err := json.MarshalIndent(obj, "", " ")
 	if err != nil {
 		http.Error(rw, fmt.Sprintf("Ошибка при переводе в json: %v", err), http.StatusInternalServerError)
 		return
